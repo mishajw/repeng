91307d54dd99db59ef03792bce38a26e5096fe2b
Misha Wagner <mishajw@gmail.com>
2024-01-11 12:24:52 +0000

    Clean up notebook.

--- experiments/repe_repro.py
+++ experiments/repe_repro.py
@@ -5,37 +5,18 @@ from pathlib import Path
 import numpy as np
 import seaborn as sns
 import torch
-import transformer_lens
-from ctransformers import AutoModelForCausalLM
 from mppr import mppr
 from sklearn.decomposition import PCA
-from torchtyping import TensorType, patch_typeguard
-from transformers import AutoTokenizer, GPTNeoXForCausalLM
+from torchtyping import TensorType
 
+from repeng import models
 from repeng.activations import ActivationRow, get_activations
 from repeng.data.true_false import TrueFalseRow, get_true_false_dataset
-
-patch_typeguard()
-
-# %%
-layer = "blocks.13.hook_resid_post"
-model_tl = transformer_lens.HookedTransformer.from_pretrained(
-    # "gpt2",
-    "pythia-1b",
-    device="cpu",
-)
-_, cache = model_tl.run_with_cache("test")
-print(list(cache.keys()))
-assert layer in cache.keys()
+from repeng.hooks.patch import patch
 
 # %%
-model = AutoModelForCausalLM.from_pretrained(
-    # "TheBloke/Llama-2-7b-Chat-GGUF",
-    # model_file=str(Path().absolute()),
-    "../llama-2-7b-chat.Q4_K_M.gguf",
-    model_type="llama",
-    gpu_layers=0,
-)
+# model, tokenizer, points = models.gpt2()
+model, tokenizer, points = models.pythia("1b")
 
 
 # %%
@@ -55,12 +36,13 @@ def format_input(row: TrueFalseRow) -> str:
 
 input = mppr.init(
     "initial",
-    base_dir=Path("output/repe_repro_pythia-1b"),
+    base_dir=Path("output/repe_repro_pythia-2.8b"),
     init_fn=get_true_false_dataset,
     to=TrueFalseRow,
 ).limit(
-    1000,
+    500,
 )
+
 df = (
     input.map(
         "format",
@@ -69,7 +51,12 @@ df = (
     )
     .map(
         "activations",
-        lambda _, text: get_activations(model=model_tl, text=text, layers=[layer]),
+        lambda _, text: get_activations(
+            model=model,
+            tokenizer=tokenizer,
+            points=points,
+            text=text,
+        ),
         to="pickle",
     )
     .join(
@@ -80,7 +67,7 @@ df = (
         lambda row: dict(
             statement=row.input.statement,
             is_true=row.input.is_true,
-            activation=row.activation.activations[layer],
+            activation=row.activation.activations["h10"],
             logprobs=row.activation.token_logprobs,
         )
     )
@@ -98,9 +85,9 @@ print(
 
 # %%
 activations = np.stack(df["activation"].tolist())
-n = 100000
-indices_1 = np.random.randint(low=0, high=df.shape[0], size=n)
-indices_2 = np.random.randint(low=0, high=df.shape[0], size=n)
+num_pairs = 100_000
+indices_1 = np.random.randint(low=0, high=df.shape[0], size=num_pairs)
+indices_2 = np.random.randint(low=0, high=df.shape[0], size=num_pairs)
 activation_diffs = activations[indices_1] - activations[indices_2]
 activation_diffs = (activation_diffs - np.mean(activation_diffs, axis=0)) / np.std(
     activation_diffs, axis=0
@@ -111,29 +98,9 @@ pca.fit_transform(activation_diffs)
 df["pca"] = df["activation"].apply(
     lambda a: (pca.components_ @ a).item(),  # type: ignore
 )
+truth_activation = pca.components_.squeeze(0)
 
 sns.histplot(data=df, x="pca", hue="is_true")
-
-# %%
-# tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-# model = GPT2LMHeadModel.from_pretrained("gpt2")
-# assert isinstance(model, GPT2LMHeadModel)
-
-model = GPTNeoXForCausalLM.from_pretrained(
-    "EleutherAI/pythia-1b",
-    revision="step3000",
-)
-tokenizer = AutoTokenizer.from_pretrained(
-    "EleutherAI/pythia-1b",
-    revision="step3000",
-)
-assert isinstance(model, GPTNeoXForCausalLM)
-
-
-# %%
-print(sum(p.numel() for p in model.parameters()))
-model
-
 
 # %%
 batch, seq, hidden = None, None, None
@@ -142,7 +109,6 @@ batch, seq, hidden = None, None, None
 def patch_fn(
     tensor: TensorType["batch", "seq", "hidden"],
 ) -> TensorType["batch", "seq", "hidden"]:
-    print(tensor.shape, truth_activation.shape)
     tensor[:, :] -= truth_activation
     return tensor
 
@@ -150,38 +116,22 @@ def patch_fn(
 def generate():
     input = tokenizer.encode(
         (
-            "Q: What city is the Eiffel Tower in? A: Paris\n"
-            "Q: What city is Westminster in? A: London\n"
-            "Q: What city is the Colosseum in? A:"
+            # "Q: What city is the Eiffel Tower in? A: Paris\n"
+            # "Q: What city is Westminster in? A: London\n"
+            # "Q: What city is the Colosseum in? A:"
+            "Q: What's the capital of France? A: Paris\n"
+            "Q: What's the capital of Germany? A: Berlin\n"
+            "Q: What's the capital of Spain? A:\n"
         ),
         return_tensors="pt",
     )
     assert isinstance(input, torch.Tensor)
-    output = model.generate(
-        input,
-        # tokenizer.encode("3 + 4 = 7\n1 + 3 = 4\n6 + 2 =", return_tensors="pt"),
-        # pad_token_id=tokenizer.pad_token_id,
-        # eos_token_id=tokenizer.eos_token_id,
-        # max_new_tokens=5,
-        # temperature=0.7,
-        # do_sample=True,
-    )
+    output = model.generate(input, max_new_tokens=5)
     assert isinstance(output, torch.Tensor)
     output = output.squeeze(0)
     print(repr(tokenizer.decode(output[input.shape[1] :])))
 
 
-# generate()
-
-x = None
-try:
-    # x = model.transformer.h[9].register_forward_hook(
-    x = model.gpt_neox.layers[13].register_forward_hook(
-        lambda _, input, output: (patch_fn(output[0]), output[1])
-    )
+generate()
+with patch(model, points[-1], patch_fn):
     generate()
-finally:
-    if x is not None:
-        x.remove()
-
-# %%
