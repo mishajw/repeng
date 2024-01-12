29609dda95805f3050e70670cd527ef35f68a459
Misha Wagner <mishajw@gmail.com>
2024-01-12 16:07:25 +0000

    Add geometry of truth datasets.

--- experiments/dataset_analysis.py
+++ experiments/dataset_analysis.py
@@ -5,6 +5,7 @@ import seaborn as sns
 
 from repeng.datasets.arc import get_arc
 from repeng.datasets.common_sense_qa import get_common_sense_qa
+from repeng.datasets.geometry_of_truth import get_geometry_of_truth
 from repeng.datasets.open_book_qa import get_open_book_qa
 from repeng.datasets.race import get_race
 from repeng.datasets.true_false import get_true_false_dataset
@@ -14,6 +15,14 @@ from repeng.datasets.types import BinaryRow, PairedBinaryRow
 # %%
 binary_datasets: dict[str, BinaryRow] = {
     **get_true_false_dataset(),
+    **get_geometry_of_truth("cities"),
+    **get_geometry_of_truth("neg_cities"),
+    **get_geometry_of_truth("sp_en_trans"),
+    **get_geometry_of_truth("neg_sp_en_trans"),
+    **get_geometry_of_truth("larger_than"),
+    **get_geometry_of_truth("smaller_than"),
+    **get_geometry_of_truth("cities_cities_conj"),
+    **get_geometry_of_truth("cities_cities_disj"),
 }
 paired_binary_datasets: dict[str, PairedBinaryRow] = {
     **get_arc("challenge"),
