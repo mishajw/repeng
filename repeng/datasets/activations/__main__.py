395fa2b33496c9be75ffbab79ae1fd1233de7ee5
Misha Wagner <mishajw@gmail.com>
2024-01-16 18:48:22 +0000

    Hardcode models for dataset generation.

--- repeng/datasets/activations/__main__.py
+++ repeng/datasets/activations/__main__.py
@@ -4,7 +4,17 @@ from repeng.datasets.activations.creation import create_activations_dataset
 from repeng.models.types import LlmId
 
 
-def main(tag: str, *, num_samples_per_dataset: int, llm_ids: list[LlmId]):
+def main(tag: str, *, num_samples_per_dataset: int):
+    llm_ids: list[LlmId] = [
+        "pythia-70m",
+        "pythia-160m",
+        "pythia-410m",
+        "pythia-1b",
+        "pythia-1.4b",
+        "pythia-2.8b",
+        "pythia-6.9b",
+        "pythia-12b",
+    ]
     create_activations_dataset(
         tag=tag,
         num_samples_per_dataset=num_samples_per_dataset,
