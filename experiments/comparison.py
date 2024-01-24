b0bbad3c5547f2c469f00a7be4eb7e3b86c0d560
Misha Wagner <mishajw@gmail.com>
2024-01-24 08:41:18 +0000

    Update comparison notebook.

--- experiments/comparison.py
+++ experiments/comparison.py
@@ -7,6 +7,7 @@ from typing import get_args
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from dotenv import load_dotenv
 from mppr import MContext
 
@@ -18,7 +19,7 @@ from repeng.activations.probe_preparations import (
 from repeng.datasets.elk.types import DatasetId, Split
 from repeng.datasets.elk.utils.collections import (
     DatasetCollectionId,
-    get_dataset_ids_for_collection,
+    resolve_dataset_ids,
 )
 from repeng.evals.probes import ProbeEvalResult, evaluate_probe
 from repeng.models.llms import LlmId
@@ -30,11 +31,17 @@ assert load_dotenv()
 
 # %%
 mcontext = MContext(Path("../output/comparison"))
-activations_dataset = mcontext.download_cached(
+activations_dataset_p1 = mcontext.download_cached(
     "activations_dataset",
-    path="s3://repeng/datasets/activations/pythia_2024-01-17_v3.pickle",
+    path="s3://repeng/datasets/activations/pythia_2024-01-23_v1.pickle",
     to="pickle",
 ).get()
+activations_dataset_p2 = mcontext.download_cached(
+    "activations_dataset_p2",
+    path="s3://repeng/datasets/activations/pythia_2024-01-23_v1_pythia-6.9b.pickle",
+    to="pickle",
+).get()
+activations_dataset = activations_dataset_p1 + activations_dataset_p2
 print(set(row.llm_id for row in activations_dataset))
 print(set(row.dataset_id for row in activations_dataset))
 print(set(row.split for row in activations_dataset))
@@ -44,17 +51,10 @@ print(set(row.split for row in activations_dataset))
 @dataclass
 class ProbeTrainSpec:
     llm_id: LlmId
-    dataset_collection_id: DatasetCollectionId
+    dataset_collection_id: DatasetId | DatasetCollectionId
     probe_id: ProbeId
     point_id: str
-
-
-# def get_evenly_spaced_points(llm_id: LlmId) -> list[str]:
-#     points = get_points(llm_id)
-#     percentiles = [0.25, 0.5, 0.7, 0.8, 0.9, 1]
-#     indices = [int((len(points) - 1) * percentile) for percentile in percentiles]
-#     indices = sorted(list(set(indices)))
-#     return [points[index].name for index in indices]
+    token_idx_from_back: int
 
 
 # %%
@@ -77,17 +77,32 @@ point_ids_by_llm = {
     }
     for llm_id in llm_ids
 }
-dataset_collection_ids: list[DatasetCollectionId] = [
+dataset_collection_ids: list[DatasetId | DatasetCollectionId] = [
     # "all",
     # "representation-engineering",
-    "geometry-of-truth",
-    "geometry-of-truth-cities",
+    # "geometry-of-truth",
     "geometry-of-truth-cities-with-neg",
-    # "persona",
+    "geometry-of-truth-cities-with-neg",
+    "arc_challenge",
+    "arc_easy",
+    "geometry_of_truth-cities",
+    "geometry_of_truth-sp_en_trans",
+    "geometry_of_truth-neg_sp_en_trans",
+    "geometry_of_truth-larger_than",
+    "geometry_of_truth-smaller_than",
+    "geometry_of_truth-cities_cities_conj",
+    "geometry_of_truth-cities_cities_disj",
+    "common_sense_qa",
+    "open_book_qa",
+    "race",
+    "truthful_qa",
+    "truthful_model_written",
+    "true_false",
 ]
 probe_ids: list[ProbeId] = [
-    # "lat",
+    "lat",
     "mmp",
+    "mmp-iid",
 ]
 probe_train_specs = mcontext.create(
     {
@@ -96,6 +111,7 @@ probe_train_specs = mcontext.create(
             dataset_collection_id=dataset_collection_id,
             probe_id=probe_id,
             point_id=point_id,
+            token_idx_from_back=1,
         )
         for llm_id, dataset_collection_id, probe_id in itertools.product(
             llm_ids,
@@ -113,13 +129,14 @@ def prepare_probe_arrays(
     dataset_ids: list[DatasetId],
     split: Split,
     point_name: str,
+    token_idx_from_back: int,
 ) -> ProbeArrays:
     return prepare_activations_for_probes(
         [
             Activation(
                 dataset_id=row.dataset_id,
                 pair_id=row.pair_id,
-                activations=row.activations[point_name],
+                activations=row.activations[point_name][token_idx_from_back],
                 label=row.label,
             )
             for row in activations_dataset
@@ -136,9 +153,10 @@ probes = probe_train_specs.map_cached(
         spec.probe_id,
         prepare_probe_arrays(
             spec.llm_id,
-            get_dataset_ids_for_collection(spec.dataset_collection_id),
+            resolve_dataset_ids(spec.dataset_collection_id),
             split="train",
             point_name=point_ids_by_llm[spec.llm_id][spec.point_id],
+            token_idx_from_back=spec.token_idx_from_back,
         ),
     ),
     to="pickle",
@@ -154,11 +172,7 @@ class ProbeEvalSpec:
 
 
 evaluation_dataset_ids = sorted(
-    set(
-        row.dataset_id
-        for row in activations_dataset
-        if row.split == "validation" and row.dataset_id.startswith("geometry")
-    )
+    set(row.dataset_id for row in activations_dataset if row.split == "validation")
 )
 
 probe_eval_specs = probes.join(
@@ -185,6 +199,7 @@ probe_evaluations = probe_eval_specs.map_cached(
             point_name=point_ids_by_llm[eval_spec.train_spec.llm_id][
                 eval_spec.train_spec.point_id
             ],
+            token_idx_from_back=eval_spec.train_spec.token_idx_from_back,
         ).labeled,
     ),
     to=ProbeEvalResult,
@@ -209,6 +224,79 @@ df = df.sort_values("llm_id")
 # dims = llm_id, dataset_collection_id, probe_id, point_id, eval_dataset_id
 df  # type: ignore
 
+# %% plot ROC curves
+df_subset = df.copy()
+df_subset = df_subset[df_subset["point_id"] == "p100"]
+df_subset = df_subset.drop(columns=["point_id"])
+df_subset = df_subset[df_subset["llm_id"] == "pythia-6.9b"]
+df_subset = df_subset.drop(columns=["llm_id"])  # type: ignore
+df_subset["probe_method"] = (
+    df_subset["probe_id"] + "+" + df_subset["dataset_collection_id"]
+)
+df_subset = df_subset.drop(columns=["probe_id", "dataset_collection_id"])
+
+fig, axs = plt.subplots(nrows=4, ncols=4, figsize=(4 * 5, 4 * 5))
+df_subset = df_subset.sort_values("eval_dataset_id")  # type: ignore
+probe_methods = df_subset["probe_method"].unique().tolist()
+for eval_dataset_id, ax in zip(df_subset["eval_dataset_id"].unique(), axs.flatten()):
+    ax.set_title(eval_dataset_id)
+    for probe_method in probe_methods:
+        df_row = df_subset[
+            (df_subset["eval_dataset_id"] == eval_dataset_id)
+            & (df_subset["probe_method"] == probe_method)
+        ].iloc[0]
+        ax.plot(
+            df_row["fprs"],
+            df_row["tprs"],
+            label=probe_method,
+        )
+        ax.plot([0, 1], [0, 1], linestyle="--", color="gray")
+
+handles, labels = [], []
+for ax in axs.flatten():
+    for handle, label in zip(*ax.get_legend_handles_labels()):
+        if label not in labels:
+            handles.append(handle)
+            labels.append(label)
+fig.legend(handles, labels)
+fig.tight_layout()
+
+
+# %% plot probe performance by model size
+df_subset = df.copy()
+df_subset = df_subset[df_subset["point_id"] == "p90"]
+df_subset = df_subset[df_subset["probe_id"] == "lat"]
+df_subset = df_subset.drop(columns=["point_id", "probe_id"])
+g = (
+    sns.FacetGrid(
+        df_subset,
+        col="eval_dataset_id",
+        col_wrap=4,
+        hue="dataset_collection_id",
+        margin_titles=True,
+    )
+    .map(sns.lineplot, "llm_id", "roc_auc_score")
+    .add_legend()
+)
+# sns.lineplot(data=df_subset, x="llm_id", y="roc_auc_score")
+# plt.xticks(rotation=90)
+# g.fig.tight_layout()
+plt.show()
+
+# %%
+df_subset = df.copy()
+df_subset = df_subset[df_subset["llm_id"] == "pythia-6.9b"]
+df_subset = df_subset[df_subset["dataset_collection_id"] == "geometry-of-truth-cities"]
+df_subset = df_subset[df_subset["point_id"] == "p100"]
+sns.barplot(
+    data=df_subset,
+    x="probe_id",
+    y="roc_auc_score",
+    hue="eval_dataset_id",
+    legend=False,
+)
+
+# %% bin
 # # %%
 # df_subset = df.copy()
 # df_subset = df_subset[df_subset["probe_id"] == "lat"]
@@ -239,31 +327,3 @@ df  # type: ignore
 # sns.lineplot(data=df_subset, x="point_id", y="f1_score", hue="llm_id")
 # plt.xticks(rotation=90)
 # plt.show()
-
-# %%
-df_subset = df.copy()
-df_subset = df_subset[df_subset["point_id"] == "p70"]
-df_subset = df_subset.drop(columns=["point_id"])
-df_subset = df_subset[df_subset["llm_id"] == "pythia-2.8b"]
-df_subset = df_subset.drop(columns=["llm_id"])  # type: ignore
-df_subset["probe_method"] = (
-    df_subset["probe_id"] + "+" + df_subset["dataset_collection_id"]
-)
-df_subset = df_subset.drop(columns=["probe_id", "dataset_collection_id"])
-print(df_subset.columns)
-
-fig, axs = plt.subplots(nrows=4, ncols=4, figsize=(4 * 5, 4 * 5))
-df_subset = df_subset.sort_values("eval_dataset_id")  # type: ignore
-for eval_dataset_id, ax in zip(df_subset["eval_dataset_id"].unique(), axs.flatten()):
-    ax.set_title(eval_dataset_id)
-    for probe_method in df_subset["probe_method"].unique():
-        df_row = df_subset[
-            (df_subset["eval_dataset_id"] == eval_dataset_id)
-            & (df_subset["probe_method"] == probe_method)
-        ].iloc[0]
-        ax.plot(
-            df_row["fprs"],
-            df_row["tprs"],
-            label=probe_method,
-        )
-    ax.legend()
