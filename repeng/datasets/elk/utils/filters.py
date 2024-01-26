0d3b49084f0e4d297679a6505b99f000f1c3eed1
Misha Wagner <mishajw@gmail.com>
2024-01-26 08:59:15 +0000

    Add val+train limits.

--- repeng/datasets/elk/utils/filters.py
+++ repeng/datasets/elk/utils/filters.py
@@ -11,12 +11,17 @@ class _DatasetAndSplit:
     split: Split
 
 
-def limit_dataset_and_split_fn(limit: int) -> Callable[[str, BinaryRow], bool]:
+def limit_dataset_and_split_fn(
+    *,
+    train_limit: int,
+    validation_limit: int,
+) -> Callable[[str, BinaryRow], bool]:
     counts: dict[_DatasetAndSplit, int] = defaultdict(int)
 
     def fn(_: str, row: BinaryRow) -> bool:
         dataset_and_split = _DatasetAndSplit(dataset_id=row.dataset_id, split=row.split)
         counts[dataset_and_split] += 1
+        limit = train_limit if row.split == "train" else validation_limit
         return counts[dataset_and_split] <= limit
 
     return fn
