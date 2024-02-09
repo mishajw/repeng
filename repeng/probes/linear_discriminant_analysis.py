d7d049bc6d880a80c55646e5e0664926a12e5dab
Misha Wagner <mishajw@gmail.com>
2024-02-09 15:19:07 +0000

    Fix LDA.

--- repeng/probes/linear_discriminant_analysis.py
+++ repeng/probes/linear_discriminant_analysis.py
@@ -7,28 +7,17 @@ See LDA probes in <https://arxiv.org/abs/2312.01037v1> and MMP-IID described in
 
 import numpy as np
 from jaxtyping import Bool, Float
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 from repeng.probes.base import DotProductProbe
 
 
-# TODO: Finding the inverse of the covariance matrix is really slow on big datasets and
-# and big hidden dimensions. Speed this up!
 def train_lda_probe(
     *,
     activations: Float[np.ndarray, "n d"],  # noqa: F722
     labels: Bool[np.ndarray, "n"],  # noqa: F821
 ) -> DotProductProbe:
-    _, hidden_dim = activations.shape
-    mean_true = activations[labels].mean(axis=0)
-    mean_false = activations[~labels].mean(axis=0)
-    direction = mean_true - mean_false
-    centered_activations = np.concatenate(
-        [
-            activations[labels] - mean_true,
-            activations[~labels] - mean_false,
-        ]
-    )
-    cov = np.cov(centered_activations, rowvar=False)
-    assert cov.shape == (hidden_dim, hidden_dim), cov.shape
-    inv_cov = np.linalg.inv(cov)
-    return DotProductProbe(inv_cov.T @ direction)
+    lda = LinearDiscriminantAnalysis()
+    lda.fit(activations, labels)
+    coefficients = lda.coef_[0]
+    return DotProductProbe(coefficients)
