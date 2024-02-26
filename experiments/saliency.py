b60fd6f16235432bbf08ab25a15381336f8a61a2
Misha Wagner <mishajw@gmail.com>
2024-02-26 08:36:33 +0000

    Update saliency notebook.

--- experiments/saliency.py
+++ experiments/saliency.py
@@ -1,14 +1,11 @@
 # %%
-import math
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 from pathlib import Path
 from typing import Sequence, cast
 
 import numpy as np
 import pandas as pd
 import plotly.express as px
-import scipy.stats
-from jaxtyping import Float
 from mppr import MContext
 from pydantic import BaseModel
 from sklearn.decomposition import PCA
@@ -16,7 +13,6 @@ from sklearn.decomposition import PCA
 from repeng.activations.probe_preparations import ActivationArrayDataset
 from repeng.datasets.activations.types import ActivationResultRow
 from repeng.datasets.elk.utils.filters import DatasetFilter, DatasetIdFilter
-from repeng.evals.logits import eval_logits_by_question, eval_logits_by_row
 from repeng.evals.probes import eval_probe_by_question
 from repeng.models.points import get_points
 from repeng.models.types import LlmId
@@ -44,16 +40,20 @@ MODEL_FAMILIES: dict[LlmId, str] = {
     "Mistral-7B-Instruct": "Mistral-7B",
 }
 FIRST_LAYERS: dict[str, int] = {
-    "Llama-2-7b": 13,
-    "Llama-2-13b": 13,
+    # "Llama-2-7b": 13,
+    # "Llama-2-13b": 13,
+    # "Mistral-7B": 4,
 }
 LAST_LAYERS: dict[str, int] = {
-    "Llama-2-7b": 32,
+    "Llama-2-7b": 29,
     "Llama-2-13b": 40,
+    "gemma-2b": 29,
+    "gemma-7b": 29,
+    "Mistral-7B": 32,
 }
 
 # %%
-output = Path("output/saliency")
+output = Path("../output/saliency")
 mcontext = MContext(output)
 activation_results_p1: list[ActivationResultRow] = mcontext.download_cached(
     "activations_results_v3",
@@ -174,192 +174,92 @@ def _eval_probe(probe: BaseProbe, spec: Spec) -> PipelineResultRow:
 
 
 # %%
-@dataclass
-class LogprobEvalSpec:
-    llm_id: LlmId
-    dataset: DatasetFilter
-
-
-class LogprobsPipelineResultRow(BaseModel, extra="forbid"):
-    llm_id: LlmId
-    dataset: str
-    accuracy: float
-
-
-def run_logprobs_pipeline(
-    llm_ids: list[LlmId],
-    datasets: Sequence[DatasetFilter],
-) -> list[LogprobsPipelineResultRow]:
-    return (
-        mcontext.create(
-            {
-                f"{llm_id}-{eval_dataset}": LogprobEvalSpec(llm_id, eval_dataset)
-                for llm_id in llm_ids
-                for eval_dataset in datasets
-            }
-        )
-        .map_cached(
-            "logprob_evaluate",
-            lambda _, spec: _eval_logprobs(spec),
-            to="pickle",
-        )
-        .get()
-    )
-
-
-def _eval_logprobs(spec: LogprobEvalSpec) -> LogprobsPipelineResultRow:
-    arrays = dataset.get(
-        llm_id=spec.llm_id,
-        dataset_filter=spec.dataset,
-        split="validation",
-        point_name="logprobs",
-        token_idx=-1,
-        limit=None,
-    )
-    row_result = eval_logits_by_row(
-        logits=arrays.activations,
-        labels=arrays.labels,
-    )
-    question_result = None
-    if arrays.groups is not None:
-        question_result = eval_logits_by_question(
-            logits=arrays.activations,
-            labels=arrays.labels,
-            groups=arrays.groups,
-        )
-    return LogprobsPipelineResultRow(
-        llm_id=spec.llm_id,
-        dataset=spec.dataset.get_name(),
-        accuracy=question_result.accuracy if question_result else row_result.accuracy,
-    )
-
-
-# %%
 """
 Pipeline for calculating saliency.
 """
-COMPONENT_INDICES: list[int] = [
-    *range(1, 16),
-    *[2**i for i in range(int(math.log(16, 2)) + 1, int(math.log(1024, 2)))],
-    1024,
-]
+NUM_COMPONENTS: int = 1024
 
 
 @dataclass
-class PcaSubset:
-    components: Float[np.ndarray, "n d"]  # noqa: F722
-    spec: Spec
+class PcaSpec:
+    llm_id: LlmId
+    dataset: DatasetFilter
+    point_name: str
 
 
-class PcaPipelineResultRow(BaseModel, extra="forbid"):
+@dataclass
+class PcaResult:
     llm_id: LlmId
     dataset: str
     point_name: str
-    num_components: int
-    accuracy: float
-    accuracy_n: int
+    saliency: int
 
 
 def run_pca_pipeline(
     llm_ids: list[LlmId],
-    train_datasets: Sequence[DatasetFilter],
-) -> list[PcaPipelineResultRow]:
-    train_specs = mcontext.create(
-        {
-            "-".join([llm_id, str(train_dataset), point.name]): Spec(
-                llm_id=llm_id,
-                dataset=train_dataset,
-                probe_method="pca",
-                point_name=point.name,
-            )
-            for llm_id in llm_ids
-            for train_dataset in train_datasets
-            for point in get_points(llm_id)[1::4]
-        }
-    )
-    pca_subsets = (
-        train_specs.map_cached(
-            "pca-v2",
-            lambda _, spec: PCA(n_components=max(COMPONENT_INDICES)).fit(
-                dataset.get(
-                    llm_id=spec.llm_id,
-                    dataset_filter=spec.dataset,
-                    split="train",
-                    point_name=spec.point_name,
-                    token_idx=-1,
-                    limit=None,
-                ).activations
-            ),
-            to="pickle",
-        )
-        .join(train_specs, lambda _, pca, spec: (pca, spec))
-        .flat_map(
-            lambda llm_id, args: {
-                f"{llm_id}-{i}": PcaSubset(
-                    components=args[0].components_[:i], spec=args[1]
+    datasets: Sequence[DatasetFilter],
+) -> list[PcaResult]:
+    return (
+        mcontext.create(
+            {
+                "-".join([llm_id, str(train_dataset), point]): PcaSpec(
+                    llm_id=llm_id,
+                    dataset=train_dataset,
+                    point_name=point,
                 )
-                for i in COMPONENT_INDICES
+                for llm_id in llm_ids
+                for train_dataset in datasets
+                for point in _get_points(llm_id)
             }
         )
-    )
-    return (
-        pca_subsets.map_cached(
-            "pca-train",
-            lambda _, pca_subset: _train_probe_on_pca_subset(pca_subset),
-            to="pickle",
-        )
-        .join(pca_subsets, lambda _, probe, pca_subsets: (probe, pca_subsets))
         .map_cached(
-            "pca-eval",
-            lambda _, args: _eval_probe_on_pca_subset(args[0], args[1]),
-            to=PcaPipelineResultRow,
+            "pca-stds",
+            lambda _, spec: _compare_stds(spec),
+            "pickle",
         )
         .get()
     )
 
 
-def _train_probe_on_pca_subset(pca_subset: PcaSubset) -> BaseProbe:
+def _get_points(llm_id: LlmId) -> list[str]:
+    points = [point.name for point in get_points(llm_id)]
+    points_skipped = points[1::2]
+    if points[-1] not in points_skipped:
+        points_skipped.append(points[-1])
+    return points_skipped
+
+
+def _compare_stds(spec: PcaSpec) -> PcaResult:
     arrays = dataset.get(
-        llm_id=pca_subset.spec.llm_id,
-        dataset_filter=pca_subset.spec.dataset,
+        llm_id=spec.llm_id,
+        dataset_filter=spec.dataset,
         split="train",
-        point_name=pca_subset.spec.point_name,
+        point_name=spec.point_name,
         token_idx=-1,
         limit=None,
     )
-    activations = arrays.activations @ pca_subset.components.T
-    return train_lr_probe(
-        activations=activations,
+    lr_probe = train_lr_probe(
+        activations=arrays.activations,
         labels=arrays.labels,
     )
+    pca = PCA(n_components=NUM_COMPONENTS).fit(arrays.activations)
 
-
-def _eval_probe_on_pca_subset(
-    probe: BaseProbe, pca_subset: PcaSubset
-) -> PcaPipelineResultRow:
     arrays = dataset.get(
-        llm_id=pca_subset.spec.llm_id,
-        dataset_filter=pca_subset.spec.dataset,
+        llm_id=spec.llm_id,
+        dataset_filter=spec.dataset,
         split="validation",
-        point_name=pca_subset.spec.point_name,
+        point_name=spec.point_name,
         token_idx=-1,
         limit=None,
     )
-    activations = arrays.activations @ pca_subset.components.T
-    assert arrays.groups is not None
-    question_result = eval_probe_by_question(
-        probe,
-        activations=activations,
-        labels=arrays.labels,
-        groups=arrays.groups,
-    )
-    return PcaPipelineResultRow(
-        llm_id=pca_subset.spec.llm_id,
-        dataset=pca_subset.spec.dataset.get_name(),
-        point_name=pca_subset.spec.point_name,
-        num_components=pca_subset.components.shape[0],
-        accuracy=question_result.accuracy,
-        accuracy_n=question_result.n,
+    pca_stds = pca.transform(arrays.activations).std(axis=0)
+    lr_std = (arrays.activations @ lr_probe.model.coef_.T).std(axis=0)
+
+    return PcaResult(
+        llm_id=spec.llm_id,
+        dataset=spec.dataset.get_name(),
+        point_name=spec.point_name,
+        saliency=np.sum(pca_stds > lr_std),
     )
 
 
@@ -396,22 +296,25 @@ fig = px.line(
     height=500,
 )
 fig.update_layout(yaxis_tickformat=".0%")
-fig.write_image(output / "1_lr_v_pca.png")
+fig.write_image(output / "1_lr_v_pca.png", scale=3)
 fig.show()
 
 
 # %%
+"""
+Plot saliency measures for a range of models.
+"""
 llm_ids: list[LlmId] = [
     "Llama-2-7b-hf",
     "Llama-2-7b-chat-hf",
     "Llama-2-13b-hf",
     "Llama-2-13b-chat-hf",
-    # "gemma-2b",
-    # "gemma-2b-it",
-    # "gemma-7b",
-    # "gemma-7b-it",
-    # "Mistral-7B",
-    # "Mistral-7B-Instruct",
+    "gemma-2b",
+    "gemma-2b-it",
+    "gemma-7b",
+    "gemma-7b-it",
+    "Mistral-7B",
+    "Mistral-7B-Instruct",
 ]
 datasets = [
     DatasetIdFilter("boolq"),
@@ -422,52 +325,24 @@ datasets = [
 
 results = run_pca_pipeline(
     llm_ids=llm_ids,
-    train_datasets=datasets,
+    datasets=datasets,
 )
-df_pca = pd.DataFrame([r.model_dump() for r in results])
-
-results = run_pipeline(
-    llm_ids=llm_ids,
-    train_datasets=datasets,
-    probe_methods=["lr"],
-)
-df_lr = (
-    pd.DataFrame([r.model_dump() for r in results])
-    .set_index(["llm_id", "dataset", "point_name"])
-    .rename({"accuracy": "threshold", "accuracy_n": "threshold_n"}, axis=1)
-    .drop(columns=["probe_method"])
-)
-
-df = df_pca.join(df_lr, on=["llm_id", "dataset", "point_name"])
-diff = df["accuracy"] - df["threshold"]
-diff_stderr = np.sqrt(
-    (df["accuracy"] * (1 - df["accuracy"]) / df["accuracy_n"])
-    + (df["threshold"] * (1 - df["threshold"]) / df["threshold_n"])
-)
-df["diff_upper"] = diff + diff_stderr * scipy.stats.norm.ppf(0.95)
-
+df = pd.DataFrame([asdict(r) for r in results])
 df["type"] = np.where(df["llm_id"].isin(CHAT_MODELS), "chat", "base")
 df["family"] = df["llm_id"].map(MODEL_FAMILIES)
 df["layer"] = df["point_name"].str.extract(r"h(\d+)").astype(int)
 df["layer"] = df.apply(
-    lambda r: (
-        (r.layer - FIRST_LAYERS.get(r.family, 0))
-        / (LAST_LAYERS.get(r.family, 32) - FIRST_LAYERS.get(r.family, 0))
-    ),
+    lambda r: r.layer / len(get_points(r.llm_id)),
     axis=1,
 )
 df = df.query("layer >= 0")
+df["saliency_rank"] = df["saliency"] + 1
+df = df.query("saliency_rank < 1000")
 
-# %%
 fig = px.line(
-    df.sort_values(["num_components"])
-    .query("diff_upper > 0")
-    .groupby(["llm_id", "dataset", "point_name"])
-    .first()
-    .reset_index()
-    .sort_values("layer"),
+    df.sort_values("layer"),
     x="layer",
-    y="num_components",
+    y="saliency_rank",
     color="type",
     facet_col="dataset",
     facet_row="family",
@@ -475,15 +350,17 @@ fig = px.line(
         "family": [
             "Llama-2-7b",
             "Llama-2-13b",
-            # "gemma-2b",
-            # "gemma-7b",
-            # "Mistral-7B",
+            "gemma-2b",
+            "gemma-7b",
+            "Mistral-7B",
         ],
         "type": ["base", "chat"],
     },
     markers=True,
     width=800,
-    height=500,
+    height=1000,
 )
-fig.write_image(output / "2_saliency.png")
+fig.update_yaxes(matches=None)
+fig.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True))
+fig.write_image(output / "2_saliency.png", scale=3)
 fig.show()
